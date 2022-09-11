64f070b6359fc60f42070103222b5951a87e8a6f
mounan <1173359575zmn@gmail.com>
2022-09-11 14:44:38 +0900

    run. sh

--- run.sh
+++ run.sh
@@ -1,6 +1,1 @@
-<<<<<<< Updated upstream
 ./odoo-bin --addons-path=addons --database=lidoo01  --db_user=zmn --db_password=Zmn1173359575  --db_host=lidoo.czofcjrwkkdr.ap-northeast-1.rds.amazonaws.com --db_port=5432 -i INIT --limit-memory-hard 0
-
-=======
-./odoo-bin --addons-path=addons --database=shosin  --db_user=zmn --db_password=Zmn1173359575  --db_host=lidoo.czofcjrwkkdr.ap-northeast-1.rds.amazonaws.com --db_port=5432 -i INIT --limit-memory-hard 0
->>>>>>> Stashed changes
